c6b981c01c28d98418adc2dbb1463e99320685c7
Gregory Yap <gregoryyap_97@hotmail.com>
2018-11-13 00:07:11 +0800

    Delete AssemblyInfo for Services.Test

--- BarebonesAutoMapperIntegration.Service.Tests/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-[assembly: System.Reflection.AssemblyCompanyAttribute("BarebonesAutoMapperIntegration.Service.Tests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("BarebonesAutoMapperIntegration.Service.Tests")]
-[assembly: System.Reflection.AssemblyTitleAttribute("BarebonesAutoMapperIntegration.Service.Tests")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
